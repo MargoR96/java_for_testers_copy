1d1c568e3db927f45c412d0ceb886d99125e24ee
MargoR96 <86987540+MargoR96@users.noreply.github.com>
2021-07-23 20:12:13 +0600

    Запуск тестов в 3 браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,17 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import ru.stqa.pft.addressbook.appmanager.ApplicatiomManager;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
-public class TestBase extends ApplicatiomManager {
+public class TestBase {
 
-  protected final ApplicatiomManager app = new ApplicatiomManager();
-  protected WebDriver wd;
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
@@ -24,24 +20,6 @@ public class TestBase extends ApplicatiomManager {
     app.stop();
 
 
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
   }
 
 }
