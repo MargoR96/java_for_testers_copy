4c9ff44627457cc952a886efe3f6ae4f9a111dcd
MargoR96 <86987540+MargoR96@users.noreply.github.com>
2021-07-29 15:44:50 +0600

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -5,9 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.NoSuchElementException;
 
 public class ContactHelper extends HelperBase {
 
